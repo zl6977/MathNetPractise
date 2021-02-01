df41ad576ed692a3fcf480f6995d78ca93ddacee
zl6977 <zl-007@126.com>
2021-02-01 15:37:42 +0100

    some new projects

---
+++ mathnetplot/mathnetplot/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace mathnetplot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            double[] dataX = new double[] { 1, 2, 3, 4, 5 };
+            double[] dataY = new double[] { 1, 4, 9, 16, 25 };
+            var plt = new ScottPlot.Plot(400, 300);
+            plt.PlotScatter(dataX, dataY);
+            plt.SaveFig("quickstart.png");
+        }
+    }
+}

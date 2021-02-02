256ec16e7281f41d86118f067e67845e051b3dd7
zl6977 <zl-007@126.com>
2021-02-02 21:47:58 +0100

    delete mathnet cookbook

--- mathnet-numerics-master/src/NativeProviders/MKL/dss.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "mkl_dss.h"
-#include "mkl_types.h"
-
-#define dss_int             MKL_INT
-#define dss_complex_float   MKL_Complex8
-#define dss_complex_double  MKL_Complex16
-

c76fc74c71b6d4c9a1af39fdda7ea6561addf90e
zl6977 <zl-007@126.com>
2021-02-04 13:33:51 +0100

    add button and datatable

--- mathnetplotform/mathnetplotform/Form1.Designer.cs
+++ mathnetplotform/mathnetplotform/Form1.Designer.cs
@@ -30,47 +30,73 @@ namespace mathnetplotform
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.formsPlot1 = new ScottPlot.FormsPlot();
             this.button_calc = new System.Windows.Forms.Button();
+            this.formsPlot1 = new ScottPlot.FormsPlot();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button_default = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // formsPlot1
-            // 
-            this.formsPlot1.AutoSize = true;
-            this.formsPlot1.Location = new System.Drawing.Point(0, 0);
-            this.formsPlot1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
-            this.formsPlot1.Name = "formsPlot1";
-            this.formsPlot1.Size = new System.Drawing.Size(832, 450);
-            this.formsPlot1.TabIndex = 0;
             // 
             // button_calc
             // 
-            this.button_calc.Location = new System.Drawing.Point(374, 487);
+            this.button_calc.Location = new System.Drawing.Point(1035, 666);
             this.button_calc.Name = "button_calc";
             this.button_calc.Size = new System.Drawing.Size(112, 34);
-            this.button_calc.TabIndex = 1;
+            this.button_calc.TabIndex = 3;
             this.button_calc.Text = "Calculate";
             this.button_calc.UseVisualStyleBackColor = true;
             this.button_calc.Click += new System.EventHandler(this.button_calc_Click);
+            // 
+            // formsPlot1
+            // 
+            this.formsPlot1.Location = new System.Drawing.Point(15, 15);
+            this.formsPlot1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.formsPlot1.Name = "formsPlot1";
+            this.formsPlot1.Size = new System.Drawing.Size(800, 685);
+            this.formsPlot1.TabIndex = 4;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(813, 30);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 32;
+            this.dataGridView1.Size = new System.Drawing.Size(356, 617);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // button_default
+            // 
+            this.button_default.Location = new System.Drawing.Point(860, 666);
+            this.button_default.Name = "button_default";
+            this.button_default.Size = new System.Drawing.Size(112, 34);
+            this.button_default.TabIndex = 6;
+            this.button_default.Text = "Default";
+            this.button_default.UseVisualStyleBackColor = true;
+            this.button_default.Click += new System.EventHandler(this.button_default_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(840, 533);
-            this.Controls.Add(this.button_calc);
+            this.ClientSize = new System.Drawing.Size(1181, 715);
+            this.Controls.Add(this.button_default);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.formsPlot1);
+            this.Controls.Add(this.button_calc);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private ScottPlot.FormsPlot formsPlot1;
         private System.Windows.Forms.Button button_calc;
+        private ScottPlot.FormsPlot formsPlot1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button_default;
     }
 }
 
--- mathnetplotform/mathnetplotform/Form1.cs
+++ mathnetplotform/mathnetplotform/Form1.cs
@@ -21,29 +21,54 @@ namespace mathnetplotform
         {
             InitializeComponent();
 
+            DataTable dt = new DataTable();
+            dataGridView1.DataSource = dt;
+            dt.Columns.Add("Item", typeof(string));
+            dt.Columns.Add("Value", typeof(double));
+            dt.Rows.Add("max_k", bendStiffener.max_k);
+            dt.Rows.Add("D1", bendStiffener.D1);
+            dt.Rows.Add("d1", bendStiffener.d1);
+            dt.Rows.Add("d2", bendStiffener.d2);
+            dt.Rows.Add("L1", bendStiffener.L1);
+            dt.Rows.Add("L2", bendStiffener.L2);
+            dt.Rows.Add("L3", bendStiffener.L3);
+            dt.Rows.Add("EIp", bendStiffener.EIp);
+            dt.Rows.Add("Es", bendStiffener.Es);
+            dt.Rows.Add("F", bendStiffener.F);
+            dt.Rows.Add("q", bendStiffener.q);
         }
+
+        BendStiffener bendStiffener = new BendStiffener
+        {
+            max_k = 0.3,
+            D1 = 0.7,
+            d1 = 0.18,
+            d2 = 0.2,
+            L1 = 0.1,
+            L2 = 2.3,
+            L3 = 0.2,
+            EIp = 1e4,
+            Es = 4.5e6,
+            F = 2e4,
+            q = 0.52
+        };
 
         private void button_calc_Click(object sender, EventArgs e)
         {
-
-
-            BendStiffener bendStiffener = new BendStiffener
-            {
-                max_k = 0.3f,
-                D1 = 0.7,
-                d1 = 0.18,
-                d2 = 0.2,
-                L1 = 0.1,
-                L2 = 2.3,
-                L3 = 0.2,
-                EIp = 1e4,
-                Es = 4.5e6,
-                F = 2e4,
-                q = 0.52
-            };
+            DataTable dt = (DataTable)dataGridView1.DataSource;
+            bendStiffener.max_k = (double)dt.Rows[0][1];
+            bendStiffener.D1 = (double)dt.Rows[1][1];
+            bendStiffener.d1 = (double)dt.Rows[2][1];
+            bendStiffener.d2 = (double)dt.Rows[3][1];
+            bendStiffener.L1 = (double)dt.Rows[4][1];
+            bendStiffener.L2 = (double)dt.Rows[5][1];
+            bendStiffener.L3 = (double)dt.Rows[6][1];
+            bendStiffener.EIp = (double)dt.Rows[7][1];
+            bendStiffener.Es = (double)dt.Rows[8][1];
+            bendStiffener.F = (double)dt.Rows[9][1];
+            bendStiffener.q = (double)dt.Rows[10][1];
 
             int N = 10000;
-
             Vector<double>[] res = bendStiffener.bvpsolver_zzz(bendStiffener.Ode_to_solve, 0.0, bendStiffener.q, 0.0, 5.0, N);
             double[] x = Vector<double>.Build.Dense(N, i => i * 5.0 / N).ToArray();
             double[] y = new double[N];
@@ -56,17 +81,46 @@ namespace mathnetplotform
                 dydx[i] = temp[1];
                 //Console.WriteLine(t[i]);
             }
+
+            formsPlot1.Reset();
             var plt = new ScottPlot.Plot(800, 600);
             //plt.PlotScatter(x, y, label: "y", markerShape: (MarkerShape)Enum.Parse(typeof(MarkerShape), "none"));
-            plt.PlotScatter(x, dydx, label: "dydx", markerShape: (MarkerShape)Enum.Parse(typeof(MarkerShape), "none"));
-            plt.Grid(false);
-            plt.Legend(fontSize: 10);
+
             //plt.PlotSignal(new[,] { x, y });
             //plt.SaveFig("quickstart.png");
 
-            formsPlot1.plt.PlotScatter(x, dydx);
-            formsPlot1.Render();
+            double[] lineX = new double[] { 0, 5 };
+            double[] lineY = new double[] { bendStiffener.max_k, bendStiffener.max_k };
+            formsPlot1.plt.PlotScatter(lineX, lineY, label: "Pass", lineWidth: 5, markerShape: (MarkerShape)Enum.Parse(typeof(MarkerShape), "none"));
 
+            double[] max_pnt = new double[] { x[Array.IndexOf(dydx, dydx.Max())], dydx.Max() };
+
+            formsPlot1.plt.PlotPoint(max_pnt[0], max_pnt[1], color: Color.Magenta, markerSize: 15);
+
+            formsPlot1.plt.PlotScatter(x, dydx, label: "dydx", lineWidth: 5, markerShape: (MarkerShape)Enum.Parse(typeof(MarkerShape), "none"));
+            //plt.PlotScatter(x, dydx, label: "dydx", markerShape: (MarkerShape)Enum.Parse(typeof(MarkerShape), "none"));
+            formsPlot1.plt.Grid(true);
+            formsPlot1.plt.Legend(fontSize: 20, location: legendLocation.lowerLeft);
+            formsPlot1.Render();
+        }
+
+        private void button_default_Click(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Item", typeof(string));
+            dt.Columns.Add("Value", typeof(double));
+            dt.Rows.Add("max_k", 0.3);
+            dt.Rows.Add("D1", 0.7);
+            dt.Rows.Add("d1",0.18 );
+            dt.Rows.Add("d2",0.2 );
+            dt.Rows.Add("L1", 0.1);
+            dt.Rows.Add("L2", 2.3);
+            dt.Rows.Add("L3", 0.2);
+            dt.Rows.Add("EIp", 1e4);
+            dt.Rows.Add("Es", 4.5e6);
+            dt.Rows.Add("F", 2e4);
+            dt.Rows.Add("q", 0.52);
+            dataGridView1.DataSource = dt;
         }
     }
 }

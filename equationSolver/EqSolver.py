23aaa47bd02c6299a6aba4efae636b4628af680a
zl6977 <zl-007@126.com>
2022-02-10 00:57:11 +0100

    Update EqSolver.py

--- equationSolver/EqSolver.py
+++ equationSolver/EqSolver.py
@@ -3,7 +3,6 @@ from itertools import count
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import interpolate
-from sqlalchemy import false
 
 x = []
 y = []
@@ -13,7 +12,7 @@ with open('data.csv', newline='') as csvfile:
     for row in data:
         [xx, yy] = row[0].split(",")
         count += 1
-        if count%1 == 0:
+        if count % 1 == 0:
             x.append(float(xx))
             y.append(float(yy))
 
@@ -29,11 +28,11 @@ def function(xIn):
 
 def df(xIn):
     if xIn == 0:
-        dx =0.0001
+        dx = 0.0001
     else:
-        dx = xIn*0.02
-    x_l = xIn -dx/2
-    x_r = xIn +dx/2
+        dx = xIn * 0.02
+    x_l = xIn - dx / 2
+    x_r = xIn + dx / 2
     y_l = function(x_l)
     y_r = function(x_r)
     dx = x_r - x_l
@@ -46,11 +45,17 @@ dy = []
 for xIn in x:
     dy.append(df(xIn) / 100)
 
-plt.figure()
-plt.plot(x, y)
-plt.plot(x, dy)
+xintpl = np.linspace(x[0], x[-1], 100)
+yintpl = []
+for xIn in xintpl:
+    yintpl.append(function(xIn))
 
-x_cur = 0.0448
+plt.figure()
+plt.plot(x, y, "x")
+plt.plot(x, dy)
+plt.plot(xintpl, yintpl)
+
+x_cur = 0.1
 iterFlag = True
 count = 0
 while (count < 1000) and iterFlag:
@@ -59,12 +64,12 @@ while (count < 1000) and iterFlag:
     count += 1
     if function(x_next) < 1e-5 and function(x_next) > -1e-5:
         iterFlag = False
-    plt.plot(x_next, function(x_next), 'x')
+    plt.plot(x_next, function(x_next), 's')
 
 print(x_next, function(x_next))
 print('count', count)
 
-plt.legend(['Cubic Spline', 'dydx', 'point'])
+plt.legend(['sample points', 'dydx', 'cubic spline', 'point'])
 # plt.axis([-0.05, 0.3, -1.05, 1.05])
 plt.title('Cubic-spline interpolation')
 plt.show()
